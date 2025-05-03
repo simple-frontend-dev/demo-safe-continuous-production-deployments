d96e36764208542ed74ba72c8515462128fa18a1
Jeremy Colin <jeremypa.colin@gmail.com>
2025-05-03 16:11:14 +0200

    ci: improve readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
+console.log("Build NETLIFY_ENV:", process.env.NETLIFY_ENV);
+console.log("Build CONTEXT:", process.env.CONTEXT);
+
 export default defineConfig({
   define: {
     "import.meta.env.PUBLIC_ENV": JSON.stringify(process.env.VERCEL_ENV || process.env.NETLIFY_ENV || "development"),
