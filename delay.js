0d139446d1fb695b52c853113403425e8ac435f1
Jeremy Colin <jeremypa.colin@gmail.com>
2025-05-03 15:46:11 +0200

    ci: clean up deployment workflows

--- delay.js
+++
@@ -1,5 +0,0 @@
-async function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-delay(30 * 1000); // one minute
--- end-to-end-tests/critical.spec.ts
+++ end-to-end-tests/critical.spec.ts
@@ -4,10 +4,11 @@ test("is up and running", async ({ page }) => {
   const pageNavigation = await page.goto("/");
 
   expect(pageNavigation?.status()).toBe(200);
-  await expect(page).toHaveTitle(
-    /Simple Frontend - Safe Continuous Production Deployments/,
-  );
-  await expect(page.getByRole("heading", { level: 3 })).toHaveText(
-    `Environment: ${process.env.PUBLIC_ENV}`,
-  );
+});
+
+test("has correct title and environment", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page).toHaveTitle(/Simple Frontend - Safe Continuous Production Deployments/);
+  await expect(page.getByRole("heading", { level: 3 })).toHaveText(`Environment: ${process.env.PUBLIC_ENV}`);
 });
